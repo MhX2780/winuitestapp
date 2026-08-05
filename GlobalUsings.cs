9e095cae73994ae9bdfcb5f9eb3a773a1958170f
Z User <z@container>
2026-08-05 14:31:35 +0000

    Add GlobalUsings.cs to fix 152 CS0246 implicit usings errors
    EnableImplicitUsings may not work in all build environments.
    Explicit global usings ensures Dictionary, List, Task, IntPtr,
    CancellationToken, Action, Func etc. are always available.

---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global usings for all project files
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.ComponentModel;
+global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
